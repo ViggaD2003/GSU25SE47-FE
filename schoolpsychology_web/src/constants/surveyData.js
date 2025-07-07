7509cbba7de50e0397ce22c34cb0a07aaa6fe818
Uyen Nguyen <khoang8666@gmail.com>
2025-07-07 18:41:10 +0700

    Refactors survey management to support survey codes
    Updates the survey management system by introducing survey codes for better identification and user experience.
    
    Removes the dependency on sample surveys in constants and implements a more dynamic approach by utilizing survey codes.
    
    Enhances the form validation by adding rules for selecting survey codes and improves the overall usability of the survey selection process.

---
+++ schoolpsychology_web/src/constants/surveyData.js
@@ -0,0 +1,10 @@
+import sampleSurveys from './sampleSurveys.json'
+
+const surveyData = sampleSurveys
+
+const surveyCode = {
+  mental_health: ['GAD-7', 'PHQ-9'],
+  environment: ['FAMILY_ENV', 'SCHOOL_ENV'],
+}
+
+export { surveyData, surveyCode }
